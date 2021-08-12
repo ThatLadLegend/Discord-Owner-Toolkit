b571cb4ddd84fa3fcfefba4609e2287d5e8a372c
Legend ModzYT <39273354+LegendModzYT@users.noreply.github.com>
2021-08-12 14:28:51 +0100

    Fixed small error. Need Sleep.

--- Cogs/info.py
+++ Cogs/info.py
@@ -13,7 +13,7 @@ class Info(commands.Cog):
 					description = "Shows info about this bot.")
 	@commands.cooldown(1, 2, commands.BucketType.member)
 	async def info(self, ctx:commands.Context):
-		embed=discord.Embed(title="Info For This Bot", description="Colour Generator is a bot that has two main features which include a `;cgen` and a `;btc`. More commands may be added later down the line but for tnow that is it.", color=randint(90, 0xffffff)
+		embed=discord.Embed(title="Info For This Bot", description="Colour Generator is a bot that has two main features which include a `;cgen` and a `;btc`. More commands may be added later down the line but for tnow that is it.", color=randint(90, 0xffffff))
 		embed.set_author(name="name", url="url", icon_url="icon")
 		embed.set_thumbnail(url="thumbnail")
 		embed.add_field(name="Open Source GitHub Page", value="This bot is Open Sourced as I don't plan to add any too difficult commands. [Click here](https://github.com/LegendModzYT/Colour-Gen-Bot)", inline=True)
