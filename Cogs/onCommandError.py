fc72dda43910d232b376170e8c67baaf56c89674
Legend ModzYT <39273354+ThatLadLegend@users.noreply.github.com>
2023-07-21 12:58:33 +0100

    Some neat new changes.

--- Cogs/onCommandError.py
+++ Cogs/onCommandError.py
@@ -1,8 +1,5 @@
-import discord
 from discord.ext import commands
 from discord.ext.commands import MissingPermissions, CheckFailure, CommandNotFound, NotOwner
-import time
-
 
 class OnCommandErrorCog(commands.Cog, name="on command error"):
 	def __init__(self, bot:commands.Bot):
@@ -15,23 +12,23 @@ class OnCommandErrorCog(commands.Cog, name="on command error"):
 			hour = round(error.retry_after/3600)
 			minute = round(error.retry_after/60)
 			if day > 0:
-				await ctx.send('This command has a cooldown, for '+str(day)+ "day(s)")
+				await ctx.reply('This command has a cooldown, for '+str(day)+ "day(s)")
 			elif hour > 0:
-				await ctx.send('This command has a cooldown, for '+str(hour)+ " hour(s)")
+				await ctx.reply('This command has a cooldown, for '+str(hour)+ " hour(s)")
 			elif minute > 0:
-				await ctx.send('This command has a cooldown, for '+ str(minute)+" minute(s)")
+				await ctx.reply('This command has a cooldown, for '+ str(minute)+" minute(s)")
 			else:
-				await ctx.send(f'This command has a cooldown, for {error.retry_after:.2f} second(s)')
+				await ctx.reply(f'This command has a cooldown, for {error.retry_after:.2f} second(s)')
 		elif isinstance(error, CommandNotFound):
 			return
 		elif isinstance(error, MissingPermissions):
- 			await ctx.send(error)
+			await ctx.reply(error)
 		elif isinstance(error, CheckFailure):
-			await ctx.send(error)
+			await ctx.reply(error)
 		elif isinstance(error, NotOwner):
-			await ctx.send(error)
+			await ctx.reply(error)
 		else:
-			print(error) 
+			await ctx.reply(error)
 
 def setup(bot):
 	bot.add_cog(OnCommandErrorCog(bot))
--- Cogs/userinfo.py
+++ Cogs/userinfo.py
@@ -2,7 +2,6 @@ import discord
 from discord.ext import commands
 from random import randint
 
-
 class UserInfo(commands.Cog):
 	def __init__(self, bot:commands.Bot):
 		self.bot = bot
@@ -15,24 +14,93 @@ class UserInfo(commands.Cog):
 		if (user == None):
 			user = ctx.author
 
-		embed=discord.Embed(title="User Info", description=f"User info for {user.mention}", color=randint(500, 0xffffff))
-		embed.add_field(name=f"Username", value=f"{user.name}", inline=True)
-		embed.add_field(name=f"User ID", value=f"{user.id}", inline=True)
+		embed = discord.Embed(title="User Info", description=f"User info for {user.mention}.", color=user.color)
+		
+		embed.add_field(name=f"Username", value=user, inline=True)
+		embed.add_field(name=f"Activity", value=f"{user.activity if user.bot is False else 'Bot Status'}", inline=True)
+		embed.add_field(name=f"User ID", value=user.id, inline=True)
+		if (user.bot == True):
+			embed.add_field(name=f"Is Bot", value=user.bot, inline=True)
+			
+		embed.add_field(name=f"Status", value=f"{'Do Not Disturb' if str(user.status).title() == 'Dnd' else str(user.status).title()}", inline=True)
+		
+		embed.add_field(name=f"Highest Role", value=user.top_role.mention, inline=True)
 
-		role = ctx.author.roles
-		role.reverse()
-		highest_role = role[0]
-		embed.add_field(name=f"Highest Role", value=f"{highest_role}", inline=True)
-
-		date_format = "%a, %d/%b/%Y"
-		embed.add_field(name=f"Created", value=f"{user.created_at.strftime(date_format)}", inline=False)
-		embed.add_field(name=f"Server Join Date", value=f"{user.joined_at.strftime(date_format)}", inline=True)
+		date_format = "%A, %d/%b/%Y"
+		embed.add_field(name = f"Account Created", value=f"<t:{int(user.created_at.timestamp())}:R>", inline=True)
+		embed.add_field(name=f"Device", value=f"{'Mobile' if user.is_on_mobile() else 'Desktop'}", inline=True)
+		embed.add_field(name = f"Server Join Date", value=f"<t:{int(user.joined_at.timestamp())}:R>", inline=True)
 
 
 		embed.set_thumbnail(url=ctx.message.author.avatar_url)
 		embed.set_footer(text=f"Requested info by {ctx.message.author}", icon_url=ctx.message.author.avatar_url)
-		await ctx.send(embed=embed)
+
+		await ctx.reply(embed=embed)
+
+class ServerInfo(commands.Cog):
+	def __init__(self, bot:commands.Bot):
+		self.bot = bot
+
+	@commands.has_permissions(manage_messages=True)
+	@commands.command(name = "Server Info", aliases = ["serverinfo", "sinfo"], usage = f"sinfo `serverID`", description = "A command which grabs the Server Information.")
+	@commands.bot_has_permissions(send_messages=True)
+	@commands.cooldown(1, 2, commands.BucketType.member)
+	async def ServerInfo(self, ctx, server: discord.Guild = None):
+		if (server == None):
+			server = ctx.guild
+
+		embed=discord.Embed(title="Server Info", description=f"## Server info for {server.name}.", color=ctx.message.author.color)
+
+		embed.add_field(name=f"Server Owner", value=server.owner, inline=True)
+		embed.add_field(name=f"Server Name", value=server.name, inline=True)
+		embed.add_field(name=f"Members", value=len(server.members), inline=True)
+		embed.add_field(name=f"Number Of Roles", value=len(server.roles), inline=True)
+
+
+		# date_format = "%A, %d/%m/%Y"
+		embed.add_field(name=f"Server Created", value=f"<t:{int(server.created_at.timestamp())}:R>", inline=False)
+
+		embed.set_thumbnail(url=server.icon_url)
+		embed.set_footer(text=f"Requested info by {ctx.message.author}.", icon_url=ctx.message.author.avatar_url)
+		embed.timestamp = ctx.message.created_at
+
+		await ctx.reply(embed=embed)
+
+
+class InviteInfo(commands.Cog):
+	def __init__(self, bot:commands.Bot):
+		self.bot = bot
+
+	@commands.command(name = "Invite Info", aliases = ["invinfo", "inviteinfo", "invite"], usage = f"invite", description = "A command which grabs the information of a valid Server invite..")
+	@commands.bot_has_permissions(send_messages=True)
+	@commands.cooldown(1, 2, commands.BucketType.member)
+	async def ServerInfo(self, ctx, invite: discord.Invite = None):
+		if (invite == None):
+			await ctx.reply("Please provide a valid invite.")
+
+		embed=discord.Embed(title="Invite Info", description=f"### Server Invite for {invite.guild}.", color=ctx.message.author.color)
+
+		embed.add_field(name=f"Server Name", value=invite.guild, inline=True)
+
+		embed.add_field(name=f"Invite ID", value=invite.id, inline=True)
+		
+		embed.add_field(name=f"Invite Channel", value=f"<#{invite.channel.id}>", inline=True)
+
+		embed.add_field(name=f"Who Generated The Invite?", value=invite.inviter, inline=True)
+
+		embed.add_field(name=f"Max Invite Age", value="Infinite" if invite.max_age == 0 else invite.max_age, inline=True)
+
+		embed.add_field(name=f"Max Invite Uses", value="Infinite" if invite.max_uses == 0 else invite.max_uses, inline=True)
+
+		embed.add_field(name=f"Approximate Member Count of Invited Server", value=f"{invite.approximate_presence_count}/{invite.approximate_member_count}", inline=True)
+
+		embed.set_footer(text=f"Requested info by {ctx.message.author}.", icon_url=ctx.message.author.avatar_url)
+		embed.timestamp = invite.created_at
+
+		await ctx.reply(embed=embed)
 
 
 def setup(bot:commands.Bot):
 	bot.add_cog(UserInfo(bot))
+	bot.add_cog(ServerInfo(bot))
+	bot.add_cog(InviteInfo(bot))
